96380a87f3fd4bc699635bd86f9c34b30974d575
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2019-02-16 07:27:04 +0100

    Did a dumb by deleting last

--- src/main/java/twistedgate/immersivepoles/ModInfo.java
+++ src/main/java/twistedgate/immersivepoles/ModInfo.java
@@ -11,5 +11,5 @@ public class ModInfo{
 	public static final String DEPENDING	= "required-after:forge@[14.23.5.2768,];required-after:immersiveengineering@[0.12-88,]";
 	public static final String PROXY_CLIENT	= "twistedgate.immersivepoles.client.ClientProxy";
 	public static final String PROXY_SERVER	= "twistedgate.immersivepoles.common.CommonProxy";
-	public static final String CERT_PRINT	= "9c558a5a794cd7f319d400240223117ab3abecf8";
+	public static final String CERT_PRINT	= "0ba25d8c0ec23537afc6db926e1ea764335a33b1";
 }
