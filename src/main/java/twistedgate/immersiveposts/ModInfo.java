0e879be4d1be314d4dbef52598f28311da1ea104
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2019-02-17 08:06:21 +0100

    Update ModInfo.java
    Dropped down to ImmersiveEngineering 0.12
    Means it should work for all 0.12 versions.

--- src/main/java/twistedgate/immersiveposts/ModInfo.java
+++ src/main/java/twistedgate/immersiveposts/ModInfo.java
@@ -8,7 +8,7 @@ public class ModInfo{
 	public static final String ID			= "immersiveposts";
 	public static final String NAME			= "Immersive Post";
 	public static final String VERSION		= "@VERSION@";
-	public static final String DEPENDING	= "required-after:forge@[14.23.5.2768,];required-after:immersiveengineering@[0.12-88,]";
+	public static final String DEPENDING	= "required-after:forge@[14.23.5.2768,];required-after:immersiveengineering@[0.12,]";
 	public static final String PROXY_CLIENT	= "twistedgate.immersiveposts.client.ClientProxy";
 	public static final String PROXY_SERVER	= "twistedgate.immersiveposts.common.CommonProxy";
 	public static final String CERT_PRINT	= "0ba25d8c0ec23537afc6db926e1ea764335a33b1";
