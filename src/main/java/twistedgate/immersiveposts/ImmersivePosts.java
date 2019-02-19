ea35947b49da678ba2fd41172a22ac717f503f68
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2019-02-19 14:50:48 +0100

    Cleaning
    Cleaning up stuff that isnt needed

--- src/main/java/twistedgate/immersiveposts/ImmersivePosts.java
+++ src/main/java/twistedgate/immersiveposts/ImmersivePosts.java
@@ -2,7 +2,6 @@ package twistedgate.immersiveposts;
 
 import org.apache.logging.log4j.Logger;
 
-import blusunrize.immersiveengineering.common.IEContent;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
@@ -17,26 +16,25 @@ import twistedgate.immersiveposts.common.blocks.IPStuff;
 @Mod(
 	modid=ModInfo.ID,
 	name=ModInfo.NAME,
-//	version=ModInfo.VERSION, // Disabled because replacing stuff aint working
 	dependencies=ModInfo.DEPENDING,
 	certificateFingerprint=ModInfo.CERT_PRINT,
 	updateJSON=ModInfo.UPDATE_URL
 )
-public class ImmersivePost{
+public class ImmersivePosts{
 	@Mod.Instance(ModInfo.ID)
-	public static ImmersivePost instance;
+	public static ImmersivePosts instance;
 	
 	@SidedProxy(modId=ModInfo.ID, serverSide=ModInfo.PROXY_SERVER, clientSide=ModInfo.PROXY_CLIENT)
 	public static CommonProxy proxy;
 	
 	public static Logger log;
 	
-	public static CreativeTabs ipCreativeTab=new CreativeTabs(ModInfo.ID){
+	public static final CreativeTabs ipCreativeTab=new CreativeTabs(ModInfo.ID){
 		ItemStack displayItem=null;
 		@Override
 		public ItemStack getTabIconItem(){
 			if(this.displayItem==null)
-				displayItem=new ItemStack(IEContent.blockWoodenDevice1,1,3);
+				displayItem=new ItemStack(IPStuff.postBase);
 				// Only temporarely using IE's wooden post
 				// Until i figure out the deal with the Extendable Post item
 			
@@ -48,13 +46,7 @@ public class ImmersivePost{
 	public void preInit(FMLPreInitializationEvent event){
 		log=event.getModLog();
 		
-		IPStuff.init();
-		
-		/*
-		 * Maybe i'll use OBJs at some point
-		 * since JSON isnt being much of a help thing
-		 */
-		//OBJLoader.INSTANCE.addDomain(ModInfo.ID);
+//		IPStuff.init();
 		
 		proxy.preInit(event);
 	}
